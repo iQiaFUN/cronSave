be0156cbca8e261885829e8e185dc5016c2fc9b1
iQiaFUN <github@iqia.fun>
2022-05-20 14:11:28 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const cron = require('node-cron');
 const path = require('path');
-const cfg = JSON.parse(NIL.IO.readFrom(path.join(__dirname, 'config.json')));
+const tmpcfg = JSON.parse(NIL.IO.readFrom(path.join(__dirname, 'example.json')));
 
 function checkFile(file, text) {
     if (NIL.IO.exists(path.join(__dirname, file)) == false) {
         NIL.IO.WriteTo(path.join(__dirname, file), text);
     }
 }
+
+checkFile("config.json", tmpcfg);
+const cfg = JSON.parse(NIL.IO.readFrom(path.join(__dirname, 'config.json')));
 
 class CronSave extends NIL.ModuleBase{
 	onStart(api){
