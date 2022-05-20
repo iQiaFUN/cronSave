b71b92354ae1b410252c7204771253fe1c9513b3
iQiaFUN <github@iqia.fun>
2022-05-20 16:25:30 +0800

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function checkFile(file, text) {
     }
 }
 
-checkFile("config.json", tmpcfg);
+checkFile("config.json", JSON.stringify(tmpcfg, null, '\t'));
 const cfg = JSON.parse(NIL.IO.readFrom(path.join(__dirname, 'config.json')));
 
 class CronSave extends NIL.ModuleBase{
